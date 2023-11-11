9c659614fb39a131ed70772678f903b641d55886
mtriSE <mtri0162@gmail.com>
2023-11-11 16:36:34 +0700

    (Update src/app_utils.py) add template

--- src/app_utils.py
+++ src/app_utils.py
@@ -2,13 +2,68 @@
 Module provide helper function 
 """
 
-class ConnectionManager:
-    def establish_connection(self, address): 
-        # Establishes a connection to the specified address.
+class FileSharingProtocol:
+    def serialize(self):
+        """
+        Converts data into a format suitable for transmission.
+        """
         pass
-    def close_connection(self, socket): 
-        # Closes a connection.
+    def deserialize(self):
+        """
+        Converts received data back into its original format.
+        """
         pass
-    def start_thread(self, target, args): 
-        # Starts a new thread for a specified target function with arguments.
+    def publish_request(self):
+        """
+        Generates the protocol message for a publish request.
+        """
+        pass
+    def fetch_request(self):
+        """
+        Generates the protocol message for a fetch request.
+        """
+        pass
+    def discover_request(self):
+        """
+        Generates the protocol message for a discover request.
+        """
+        pass
+    def ping_request(self):
+        """
+        Generates the protocol message for a ping request.
+        """
+        pass
+
+class FileRepository:
+    def add_file(self, client, file):
+        """
+        Adds a file to the repository for a specific client.
+        """
+        pass
+    def get_files(self, client):
+        """
+        Retrieves the list of files for a specific client.
+        """
+        pass
+    def get_all_files(self):
+        """
+        Retrieves the list of all files across all clients.
+        """
+        pass
+
+class ClientFileRepository:
+    def add_local_file(self):
+        """
+        Adds a local file to the client's repository.
+        """
+        pass
+    def get_local_files(self):
+        """
+        Retrieves the list of local files on the client.
+        """
+        pass
+    def get_file_path(self):
+        """
+        Retrieves the file path for a given file name.
+        """
         pass

3518403421c9df982bb85e03371423808e7228d9
VuongKhanh(Kal) <122599235+vuongkhanh1537@users.noreply.github.com>
2023-11-13 17:41:16 +0700

    Add client file repository

--- src/client.py
+++ src/client.py
@@ -8,19 +8,24 @@ from threading import Lock      # Mutex lock
 from app_utils import *
 
 class Client:
-    def __init__(self, server_address, username):
+    def __init__(self, server_address, username, file_repository):
         self.server_address = server_address
         self.username = username
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+        self.file_repository = file_repository
 
         try:
             self.client_socket.connect(self.server_address)
             print(f"Connected to the server at {self.server_address}")
+            self.connected = False
         except Exception as e:
             print(f"Error connecting to the server: {e}")
             self.client_socket.close()
 
     def publish(self, lname, fname):
+
+        self.file_repository.add_local_file(lname, fname)
         command = f"publish {lname} {fname}"
         self.send_command(command)
 
@@ -31,6 +36,7 @@ class Client:
     def send_command(self, command):
         try:
             self.client_socket.send(command.encode())
+            # Handle server response here
         except Exception as e:
             print(f"Error sending command to the server: {e}")
 
@@ -54,10 +60,12 @@ class Client:
 def main():
     server_address = ("127.0.0.1",2000)
     username = input("Enter your username: ")
-    client = Client(server_address, username)
+    file_repository = ClientFileRepository()
+    client = Client(server_address, username, file_repository)
 
-    client_thread = threading.Thread(target=client.start)
-    client_thread.start()
+    if client.connected:
+        client_thread = threading.Thread(target=client.start)
+        client_thread.start()
 
 if __name__=="__main__":
     main()
